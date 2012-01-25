9a56f43088dd6ee051007ecbe4122dd931b40c0f
TheMassiveChipmunk <whatsmynamefs@gmail.com>
2012-01-24 19:15:09 -0800

    Updates

--- main.cpp
+++ main.cpp
@@ -3,10 +3,46 @@
 #include "Map.hpp"
 #include "TileSet.hpp"
 
+void showFlags ()
+{
+    TileSet Set ("Set.txt");
+
+    unsigned int i = 0;
+    
+    for (i = 0 ; i < Set.size () ; i++)
+    {
+	std::cerr << i;
+	if (Set [i].Type & TYPE_OPEN)
+	{
+	    std::cerr << " has the open flag on ";
+	}
+	if (Set [i].Type & TYPE_CLOSED)
+	{
+	    std::cerr << " has the closed flag on ";
+	}
+	if (Set [i].Type & TYPE_HIDE)
+	{	    
+	    std::cerr << " has the hide flag on ";
+	}	
+	if (Set [i].Type & TYPE_EVENT)
+	{	    
+	    std::cerr << " has the event flag on ";
+	}
+	if (Set [i].Type & TYPE_MOVABLE)
+	{
+	    std::cerr << " has the movable flag on ";
+	}
+	
+	std::cerr << std::endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     sf::VideoMode VMode (800 , 600 , 32);
     sf::RenderWindow Window (VMode , "Epic-Quest");
+    
+    showFlags ();
 
     //Map
     Map MyMap ("Set.txt" , "Map.txt");
