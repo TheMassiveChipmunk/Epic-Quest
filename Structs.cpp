8d5f0ed6a1023e92d27245a98acc42e57dedf345
TheMassiveChipmunk <whatsmynamefs@gmail.com>
2012-02-19 20:17:59 -0800

    Deleted some files

--- Structs.cpp
+++
@@ -1,9 +0,0 @@
-#include "Structs.hpp"
-
-//Enumeration operators
-
-//Tile type | operator
-TileType operator| (const TileType& First , const TileType& Second)
-{
-    return (TileType) ((int) First | (int) Second);
-}
