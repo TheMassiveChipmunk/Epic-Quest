74d744937c63d2ff687589a947f1432965ac0485
TheMassiveChipmunk <whatsmynamefs@gmail.com>
2012-04-06 13:04:39 -0700

    Added README

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 
 #include <vector>
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+#include <windows.h>
+#else
 #include <unistd.h>
+#endif
 
 #include "Level.hpp"
 #include <SFML/Audio.hpp>
